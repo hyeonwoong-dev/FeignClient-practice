75405ab3a0638816fc6f3b37a1731b0f57c7607b
hyeonwoong <bearoo.work@gmail.com>
2024-12-02 17:00:31 +0900

    Add Getter to Order

--- order/src/main/java/com/nbcamp/course/feign/order/model/Order.java
+++ order/src/main/java/com/nbcamp/course/feign/order/model/Order.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(access = AccessLevel.PRIVATE)
