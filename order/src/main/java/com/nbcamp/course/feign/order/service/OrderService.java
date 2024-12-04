f17397a6f1e285c1ca4d7ce5efa743acf0c2b91f
hyeonwoong <bearoo.work@gmail.com>
2024-12-04 11:55:36 +0900

    Implements create order method

--- order/src/main/java/com/nbcamp/course/feign/order/service/OrderService.java
+++ order/src/main/java/com/nbcamp/course/feign/order/service/OrderService.java
@@ -1,8 +1,10 @@
 package com.nbcamp.course.feign.order.service;
 
+import com.nbcamp.course.feign.order.client.ProductClient;
 import com.nbcamp.course.feign.order.model.Order;
 import com.nbcamp.course.feign.order.model.OrderRequestDto;
 import com.nbcamp.course.feign.order.model.OrderResponseDto;
+import com.nbcamp.course.feign.order.model.ProductResponseDto;
 import com.nbcamp.course.feign.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final ProductClient productClient;
 
     public void createOrder(OrderRequestDto request) {
         // TODO: Product service 에 productId에 맞는 상품이 있는지 확인
+        Long productId = request.getProductId();
+
+        try {
+            ProductResponseDto product = productClient.getProduct(productId);
+        } catch (Exception e) {
+            throw new RuntimeException("Product not found");
+        }
+
         // TODO: Order 객체 생성
+        Order order = Order.create(productId);
+        orderRepository.save(order);
     }
 
     public OrderResponseDto getOrder(Long id) {
