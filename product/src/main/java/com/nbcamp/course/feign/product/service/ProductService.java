9f22fd74b782c260fd75ad4c377b82b6a16762b3
hyeonwoong <bearoo.work@gmail.com>
2024-12-04 11:21:30 +0900

    add final

--- product/src/main/java/com/nbcamp/course/feign/product/service/ProductService.java
+++ product/src/main/java/com/nbcamp/course/feign/product/service/ProductService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     @PostConstruct
     private void init() {
