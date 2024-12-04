71d61b1c021d84115698b7823f12f54d944236b3
hyeonwoong <bearoo.work@gmail.com>
2024-12-04 12:03:11 +0900

    FeignClient header 1 - requestInterceptor

---
+++ order/src/main/java/com/nbcamp/course/feign/order/client/ProductFeignClientConfig.java
@@ -0,0 +1,25 @@
+package com.nbcamp.course.feign.order.client;
+
+import feign.Logger;
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class ProductFeignClientConfig {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header("server-port", serverPort);
+            requestTemplate.header("USER-ROLE", "CUSTOMER");
+            requestTemplate.header("INTERNAL-KEY", "something key");
+        };
+    }
+}
